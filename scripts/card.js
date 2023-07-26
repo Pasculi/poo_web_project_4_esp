3071db429c7a653877271bfd9f914057be455f1e
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2023-07-25 22:50:57 -0400

    feat: Creando archivo Card

--- scripts/card.js
+++ scripts/card.js
@@ -1,19 +1,33 @@
-export default class Card{
-  constructor(data, template) {
+export default class Card {
+  constructor(data, cardSelector) {
     this._name = data.name;
     this._link = data.link;
-    this._template = template;
+    this._cardSelector = cardSelector;
   }
-  _getTemplateCard() {
-    const cardContainer = document.querySelector(this._template).content;//Donde se renderizarán las Card
-    console.log("🚀 ~ file: card.js:9 ~ Card ~ _getTemplateCard ~ cardContainer:", cardContainer)
-    const templatecard = cardContainer.querySelector('.card').cloneNode(true);//Seleccionamos la etiqueta template
+  _getTemplate() {
+    const cardContainer = document.querySelector(".container-card");//Donde se renderizarán las Card
+    const templatecard = cardContainer.querySelector('.card');//Seleccionamos la etiqueta template
+
+
     return templatecard
   }
-  renderCard() {
-    this._cardElement = this._getTemplateCard();
-    
+  generateCard() {
+    this._element = this._getTemplate();
+    // Añadir datos
+    this._element.querySelector('.card__place-name').textContent = this._name;
+    this._element.querySelector('.card__place-image-place').setAttribute("src", this._link);
+    this._element.querySelector('.card__place-image-place').setAttribute("alt", this._name);
+
+
+    // Devolver el elemento
+    return this._element;
+
   }
 
 }
 
+initialCards.forEach(data => {
+  const card = new Card(data, template);
+  const cardElement = card.generateCard();
+  cardContainer.prepend(cardElement);
+});
